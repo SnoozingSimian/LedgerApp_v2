4514a375fc486652bc4615c7787b990a63458ac3
Ayan Gangopadhyay <45364686+SnoozingSimian@users.noreply.github.com>
2025-11-30 22:16:41 +0530

    Update routers __init__.py to include budgets

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,5 @@
 # app/routers/__init__.py
 
-from app.routers import auth, transactions
+from app.routers import auth, transactions, budgets
 
-__all__ = ["auth", "transactions"]
+__all__ = ["auth", "transactions", "budgets"]
